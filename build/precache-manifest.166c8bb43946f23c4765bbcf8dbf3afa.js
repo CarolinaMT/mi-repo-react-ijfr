4931d5822d810196df8bbd1548150db77dedfdea
CarolinaMTitrado <66382437+CarolinaMTitrado@users.noreply.github.com>
2020-07-26 18:20:16 -0500

    Add files via upload

---
+++ build/precache-manifest.166c8bb43946f23c4765bbcf8dbf3afa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1538835ec27c13c6a640e59b016c4db0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e40b307950b73cb4a27",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a918a0c8b962e6868945",
+    "url": "/static/js/2.477f262f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.477f262f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e40b307950b73cb4a27",
+    "url": "/static/js/main.3b35348c.chunk.js"
+  },
+  {
+    "revision": "1289086ab8185907b286",
+    "url": "/static/js/runtime-main.869e9a9e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
